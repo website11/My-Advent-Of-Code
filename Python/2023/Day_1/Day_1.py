9aa057e52e98d8e314a8f42e945eb286b0b616ba
Alexander Wang <ayw32@drexel.edu>
2023-12-08 11:23:40 -0500

    Day 1 Part 2

--- Python/2023/Day_1/Day_1.py
+++ Python/2023/Day_1/Day_1.py
@@ -1,3 +1,4 @@
+# Chance to practice using regex package
 import re
 
 
@@ -13,10 +14,48 @@ def extract_calibration_values(line):
         return value
 
 
+def convert_to_number(number):
+    # Dictionary to pair strings to numbers
+    word_dict = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8',
+                 'nine': '9'}
+    if number in word_dict.keys():
+        real_number = word_dict.get(number)
+        return real_number
+    else:
+        return str(number)
+
+
+# Part 2
+def extract_word_calib_values(line):
+    numbers = re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine',line)
+    for item in numbers:
+        # Addresses strings tied together
+        num = convert_to_number(item)
+        new_sub = item[0] + num + item[-1]
+        line = re.sub(item,new_sub,line)
+    numbers = re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line)
+    if len(numbers) == 1:
+        number = convert_to_number(numbers[0])
+        value = int(number + number)
+        return value
+    else:
+        front = convert_to_number(numbers[0])
+        back = convert_to_number(numbers[-1])
+        value = int(front + back)
+        print(value)
+        return value
+
+
 if __name__ == '__main__':
-    total = 0
+    first_total = 0
+    second_total = 0
     input_text = "input-1.txt"
     with open('input-1.txt','r') as f:
         for line in f:
-            total += extract_calibration_values(line)
-    print(total)
+            first_total += extract_calibration_values(line)
+            second_total += extract_word_calib_values(line)
+    # Part 1 solution
+    print(first_total)
+    # Part 2 solution
+    print(second_total)
+
